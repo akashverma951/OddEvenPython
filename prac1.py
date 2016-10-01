9e428721048c78031813f666acf054150bc01884
Aakash <akashvarma636@gmail.com>
2016-10-01 12:47:06 +0530

    added evenodd

---
+++ prac1.py
@@ -0,0 +1,7 @@
+# program to indentify the even/odd state of given number
+number=int(input("Enter a number:"))
+if number%2==0:
+    print ("The entered number is even")
+else:
+    print ("The entered number is odd")
+    
