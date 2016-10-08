c8380c506da69517e6766ed1a9829c7f8357633a
gaurav <shuklagaurav710@gmail.com>
2016-10-08 08:46:14 +0530

    change the program with functions1

--- prac1.py
+++ prac1.py
@@ -1,7 +1,22 @@
-# program to indentify the even/odd state of given number
-number=int(input("Enter a number:"))
-if number%2==0:
-    print ("The entered number is even")
-else:
-    print ("The entered number is odd")
-    
+#program to identify the even/odd state of given number
+#function to print OddEven of given number
+
+def even(number):
+    if number == 0:
+        return True
+    return odd(number - 1)
+
+def odd(number):
+    if number == 0:
+        return False
+    return even(number - 1)
+
+def isEven(number):
+    if even(number):
+        print("Number is even")
+    else:
+        print("Number is odd")
+
+number = int(input("enter the number"))
+
+isEven(number)
